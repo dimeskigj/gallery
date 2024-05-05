4593bb723cbe77dd68dcc4407a33caa8948d6f99
Gjorgji Dimeski <dimeskigj@gmail.com>
2024-05-06 01:25:58 +0200

    Add columns in large screens

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <div class="min-h-dvh w-full bg-slate-100 dark:bg-slate-900 flex flex-col items-center">
-	<div class="dark:text-slate-200 font-mono w-full lg:w-1/2 px-5 pt-5 pb-5 lg:px-0 text-md lg:text-3xl flex">
+	<div
+		class="dark:text-slate-200 font-mono w-full lg:w-1/2 px-5 pt-5 pb-5 lg:px-0 text-md lg:text-3xl flex"
+	>
 		<a
 			class="text-violet-500 dark:text-violet-300 hover:text-purple-400 hover:underline"
 			href="https://dimeski.net"
