946c5bf3a00d0217deb83a3ec9fa7e16b5692d80
Gjorgji Dimeski <dimeskigj@gmail.com>
2024-05-06 01:06:24 +0200

    Add thumbnail creation script

---
+++ create-thumbnails.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+input_dir="static/images/full"
+output_dir="static/images/thumbnails"
+
+rm -f $output_dir/*
+
+for file in "$input_dir"/*; do
+    if [ -f "$file" ]; then
+        filename=$(basename "$file")
+        convert "$file" -resize 700x "$output_dir/$filename"
+    fi
+done
