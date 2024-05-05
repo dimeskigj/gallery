edf55b372e72ac3b0f2cb38fee8dae2512e1fb0d
Gjorgji Dimeski <dimeskigj@gmail.com>
2024-05-06 01:46:05 +0200

    Remove static/ head

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -13,8 +13,8 @@ type Image = {
 }
 
 export function load(): { images: Image[] } {
-    const thumbnailSourcePath = 'static/images/thumbnails';
-    const fullSourcePath = 'static/images/full';
+    const thumbnailSourcePath = 'images/thumbnails';
+    const fullSourcePath = 'images/full';
     const filePaths = fs.readdirSync(thumbnailSourcePath);
 
     const images = filePaths.map(filePath => {
