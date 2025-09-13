15f55ae8cba6696006ac29d05b54633c4f7d7392
Gjorgji Dimeski <dimeskigj@gmail.com>
2025-09-13 19:08:11 +0200

    Add photos and update style

--- create-thumbnails.sh
+++ create-thumbnails.sh
@@ -8,6 +8,6 @@ rm -f $output_dir/*
 for file in "$input_dir"/*; do
     if [ -f "$file" ]; then
         filename=$(basename "$file")
-        convert "$file" -resize 400x "$output_dir/$filename"
+        convert "$file" -resize 300x "$output_dir/$filename"
     fi
 done
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.css';
 </script>
 
-<div class="min-h-dvh w-full bg-slate-100 dark:bg-slate-900 flex flex-col items-center">
+<div class="min-h-dvh w-full bg-slate-50 dark:bg-slate-950 flex flex-col items-center">
 	<div
 		class="dark:text-slate-200 font-mono w-full lg:w-1/2 px-5 pt-5 pb-5 lg:px-0 text-md lg:text-3xl flex"
 	>
@@ -15,6 +15,23 @@
 		<h1>:~/Gallery$</h1>
 	</div>
 	<div class="lg:w-1/2 w-full px-3 lg:px-0">
+		<span class="text-slate-950 dark:text-slate-50 block w-full">
+		I spend most of my time building apps and solving problems with code, but photography is where
+			I slow down and notice the small details.
+			<br class="mb-1" />
+
+			This gallery is a mix of everyday life, light, and perspective, my way of capturing the same
+			curiosity I bring into my work, but in a more visual, creative form.
+			<br class="mb-1" />
+
+			Learn more about me at
+			<a
+				href="https://dimeski.net"
+				class="text-violet-500 dark:text-violet-300 hover:text-purple-400 hover:underline"
+				>dimeski.net</a
+			>.
+		</span>
+		<div class="h-6"></div>
 		<slot />
 	</div>
 </div>
